bb67f5fbc311f633e93e6cca0549ec6677e5ff4a
Ilya Boyandin <ilya@boyandin.me>
2021-12-18 12:40:51 +0100

    License

--- packages/data/src/FlowMapSelectors.ts
+++ packages/data/src/FlowMapSelectors.ts
@@ -1,5 +1,6 @@
 /*
- * Copyright 2021 Teralytics, FlowmapBlue
+ * Copyright 2022 FlowmapBlue
+ * Copyright 2019 Teralytics, modified by FlowmapBlue
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
