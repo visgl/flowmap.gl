1b344845c5241290d9b89caad32f6509f1253441
Ilya Boyandin <ilya@boyandin.me>
2022-02-15 14:57:27 +0100

    Fix demo app initial viewState

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -46,17 +46,14 @@ function App() {
         data.locations,
         (loc: LocationDatum) => [loc.lon, loc.lat],
         [width, height],
-        {
-          // this is just for the BIXI dataset to look nicer on load
-          padding: {
-            top: -height * 0.6,
-            left: -width * 0.2,
-            bottom: -height * 0.2,
-            right: 0,
-          },
-        },
       );
-      setViewState({...viewState, width, height});
+      setViewState({
+        ...viewState,
+        latitude: viewState.latitude - 0.02,
+        zoom: viewState.zoom + 1,
+        width,
+        height,
+      });
     }
   }, [data]);
   const handleViewStateChange = ({viewState}: any) => {
