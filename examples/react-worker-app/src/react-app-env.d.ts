d877776e36f8837d74cd050693bce2ef897baa16
arashkav <arash.kaviani.a@gmail.com>
2023-02-07 10:37:09 +1100

    flowmap.gl configured

--- examples/react-worker-app/src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) Flowmap.gl contributors
- * Copyright (c) 2018-2020 Teralytics
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/// <reference types="react-scripts" />
-declare module '@deck.gl/react';
